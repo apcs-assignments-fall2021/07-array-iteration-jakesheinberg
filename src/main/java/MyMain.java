061b900a46de7e3408e4becd5993b2acf05dcd8f
jakesheinberg <23jsheinberg@athenian.org>
2021-09-23 23:30:09 -0700

    everything but second biggest

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,14 +2,38 @@ public class MyMain {
 
     // Reverses an array
     public static int[] reverse(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+       int [] arrRev = new int[arr.length];
+       int u = 0;
+       for (int i = arr.length-1; i>=0; i--){
+           arrRev[u] += arr[i];
+           u++;
+        }
+
+        return arrRev;
     }
 
     // Finds the second largest number in an array
     public static int secondLargest(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+            int u = 0;
+            int j = 0;
+            int index = 0;
+            for(int i = 0; i<arr.length; i++){
+                u = arr[i];
+                if (u>j);
+                j=u;
+                index=i;
+            }
+            int p=0;
+        int[] copy = new int[arr.length - 1];
+        for(int i = 0; i<copy.length; i++){
+                if (i != index) {
+                     copy[p++] += arr[i];}
+        }for(int i = 0; i<copy.length; i++) {
+            u = copy[i];
+            if (u > j) ;
+            j = u;
+
+        }return j;
     }
 
     // This method is given a word, and it checks to see
@@ -20,10 +44,17 @@ public class MyMain {
     public static boolean spellCheck(String word) {
         // Don't edit this array
         // This is our dictionary of valid words
+        int x=0;
         String[] dictionary = {"the","of","and","a","to","in","is","you","that","it","he","was","for","on","are","as","with","his","they","I","at","be","this","have","from","or","one","had","by","word","but","not","what","all","were","we","when","your","can","said","there","use","an","each","which","she","do","how","their","if","will","up","other","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","has","look","two","more","write","go","see","number","no","way","could","people","my","than","first","water","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","cat","dog","cats","dogs"};
+        for  (int i = 0; i< dictionary.length; i++){
+            if (word == dictionary[i]) {
+                x = 5;
+            }
 
-        // REPLACE WITH YOUR CODE HERE
+        }if(x==0){
         return false;
+    }else{
+        return true;}
     }
 
 
